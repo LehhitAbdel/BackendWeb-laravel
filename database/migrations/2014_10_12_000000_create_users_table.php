40bc8c506db7e63b6c03de4c07f4aacd24cacea5
LehhitAbdel <abdel.lehhit@student.ehb.be>
2023-12-26 16:05:35 +0100

    login basic+ dashboard

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('email');
-            $table->date('birthday')->nullable();
-            $table->string('avatar')->nullable();
-            $table->text('about_me')->nullable();
-            $table->boolean('is_admin')->default(false);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
