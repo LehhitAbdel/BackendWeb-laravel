c098da0f99f4a961008e3a0291bbad60d328b8fd
LehhitAbdel <abdel.lehhit@student.ehb.be>
2023-12-31 14:59:54 +0100

    added create post button only visible for admin

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,28 +1,45 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('News Posts') }}
-        </h2>
+        <div class="flex justify-between items-center">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('News Posts') }}
+            </h2>
+    
+            @if(auth()->check() && auth()->user()->is_admin)
+                <a href="{{ url('/news/create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Create News Post
+                </a>
+            @endif
+        </div>
     </x-slot>
+   
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <!-- Display all news posts -->
-                    @foreach ($newsPosts as $post)
-                    <div>
-                        <h2>{{ $post->title }}</h2>
-                        <img src="{{ $post->cover_image }}" alt="Cover Image">
-                        <div>{{ $post->content }}</div>
-                        <small>Published on: {{ $post->published_at}}</small>
-                        <p>Posted by: <a href="{{ route('user.profile', $post->user->id) }}" class="font-bold underline">{{ $post->user->name }}</a></p>
+                  
+                    <!-- Display all news posts in a grid -->
+                    <div class="grid md:grid-cols-2 gap-4">
+                        @foreach ($newsPosts as $post)
+                            <div class="border border-gray-200 rounded-lg p-4">
+                                <h2 class="font-bold">{{ $post->title }}</h2>
+                                <img src="{{ $post->cover_image }}" alt="Cover Image" class="w-full h-auto">
+                                <div>{{ $post->content }}</div>
+                                <small>Published on: {{ $post->published_at }}</small>
+                                <p>Posted by: <a href="{{ route('user.profile', $post->user->id) }}" class="font-bold underline">{{ $post->user->name }}</a></p>
+                            </div>
+                        @endforeach
                     </div>
-                    @endforeach
 
-                    {{ $newsPosts->links() }} <!-- Pagination links -->
+    
+                    <!-- Pagination links -->
+                    <div class="mt-4">
+                        {{ $newsPosts->links() }}
+                    </div>
                 </div>    
             </div>    
         </div>    
-    </div>    
+    </div>
+    
 
 </x-app-layout>
