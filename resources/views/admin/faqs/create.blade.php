629b18474e5bde8c03fc267cae3bf6cb848092f8
LehhitAbdel <abdel.lehhit@student.ehb.be>
2024-01-08 19:52:23 +0100

    longer dropdown

--- resources/views/admin/faqs/create.blade.php
+++ resources/views/admin/faqs/create.blade.php
@@ -5,7 +5,7 @@
         <form method="POST" action="{{ route('faqs.store') }}" class="mt-5 bg-white shadow rounded-md p-6">
             @csrf
             <div class="flex justify-between items-center mb-4">
-                <div>
+                <div class="w-1/2 mr-3">
                     <label for="faq_category_id" class="block text-sm font-medium text-gray-700 mb-2">Category:</label>
                     <select name="faq_category_id" id="faq_category_id" class="block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                         @foreach ($categories as $category)
@@ -17,6 +17,8 @@
                     Manage Categories
                 </a>
             </div>
+            
+            
 
             <div class="mb-4">
                 <label for="question" class="block text-sm font-medium text-gray-700 mb-2">Question:</label>
