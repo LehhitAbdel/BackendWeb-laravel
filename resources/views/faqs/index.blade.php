b7715c31cfe439faee577e6d38d6ee472e549533
LehhitAbdel <abdel.lehhit@student.ehb.be>
2024-01-08 19:10:29 +0100

    added create awnser button for admin

--- resources/views/faqs/index.blade.php
+++ resources/views/faqs/index.blade.php
@@ -1,8 +1,15 @@
 <x-app-layout>
     <x-slot name="header">
+        <div class="flex justify-between items-center">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('FAQ') }}
         </h2>
+        @if(auth()->check() && auth()->user()->is_admin)
+        <a href="{{ url('/faqs/create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Make an awnser
+        </a>
+        @endif
+    </div>
     </x-slot>
 
     <div class="max-w-4xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
