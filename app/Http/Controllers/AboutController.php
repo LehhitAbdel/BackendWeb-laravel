7eea69428c3e130d3cf22f6e787a63651d98bc6c
LehhitAbdel <abdel.lehhit@student.ehb.be>
2024-01-14 17:47:17 +0100

    last changes

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -15,7 +15,7 @@ class AboutController extends Controller
 {
     $sources = [
         (object) ['title' => 'My git', 'url' => 'https://github.com/LehhitAbdel/BackendWeb-laravel'],
-        (object) ['title' => 'Laravel documentation', 'url' => 'https://laravel.com'],
+        (object) ['title' => 'Laravel documentation (+breeze)', 'url' => 'https://laravel.com'],
         (object) ['title' => 'Chat GPT', 'url' => 'https://chat.openai.com'],
         (object) ['title' => 'Laravel daily (youtube)', 'url' => 'https://www.youtube.com/@LaravelDaily'],
 
